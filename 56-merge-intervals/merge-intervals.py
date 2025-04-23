fdecb746c97fb95a3958b3766a22171465d34266
Andrew Ivanov <106903166+F1ameX@users.noreply.github.com>
2025-04-23 16:58:09 +0700

    Time: 12 ms (17.07%) | Memory: 21 MB (63.24%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,10 +3,12 @@ class Solution:
         intervals.sort()
         result = [intervals[0]]
         for i in range(1, len(intervals)):
-            if intervals[i][0] <= result[-1][-1]:
-                result[-1][0] = min(result[-1][0], intervals[i][0])
-                result[-1][-1] = max(result[-1][-1], intervals[i][-1])
+            current = intervals[i]
+            last = result[-1]
+            if current[0] <= last[-1]:
+                last[0] = min(last[0], current[0])
+                last[-1] = max(last[-1], current[-1])
             else:
-                result.append(intervals[i])
+                result.append(current)
 
         return result
