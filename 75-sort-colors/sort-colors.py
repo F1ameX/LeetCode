44ddd58293c6600f5e7989ffa60553f4cdd7add9
Andrew Ivanov <106903166+F1ameX@users.noreply.github.com>
2025-04-30 12:07:29 +0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.82%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,24 +1,21 @@
 class Solution:
-    def partition(self, nums: List[int], low: int, high: int) -> int:
-        pivot = nums[high]
-
-        i = low - 1
-        for j in range(low, high):
-            if nums[j] <= pivot:
-                i = i + 1
-                nums[i], nums[j] = nums[j], nums[i]
-
-        nums[i + 1], nums[high] = nums[high], nums[i + 1]
-        return i + 1
-    
-    def quick_sort(self, nums: List[int], low: int, high: int) -> None:
-        if low < high:
-            pivot = self.partition(nums, low, high)
-            self.quick_sort(nums, low, pivot - 1)
-            self.quick_sort(nums, pivot + 1, high)
-    
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        self.quick_sort(nums, 0, len(nums) - 1)
+        zero, two, current = -1, len(nums), 0
+
+        while current < two:
+            if nums[current] == 0:
+                zero += 1
+                nums[zero], nums[current] = nums[current], nums[zero]
+                current += 1
+            
+            elif nums[current] == 2:
+                two -= 1
+                nums[two], nums[current] = nums[current], nums[two]
+            
+            else:
+                current += 1
+        
+        
