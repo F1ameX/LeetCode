6505109ff1d647200fceb531c6f8d9cc9081e123
Andrew Ivanov <106903166+F1ameX@users.noreply.github.com>
2025-07-10 14:48:10 +0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (26.82%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.flag = True
+    def check(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left = self.check(root.left)
+        right = self.check(root.right)
+        if abs(left - right) > 1:
+            self.flag = False
+        return 1 + max(left, right)
+    
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        self.check(root)
+        if self.flag:
+            return True
+        return False
+        
+        
